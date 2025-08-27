7c41e0881c565b63cd23c917bbb89897ef7199b4
dammar093 <dammarrana093@gmail.com>
2025-08-27 20:58:58 +0545

    optimized

--- frontend/src/app/(public)/properties/[name]/[id]/page.tsx
+++ frontend/src/app/(public)/properties/[name]/[id]/page.tsx
@@ -8,8 +8,10 @@ import { LuDot } from "react-icons/lu";
 import GoogleMap from "@/components/google-map/google-map";
 import { Property, User } from "@/types/property";
 import { getPropertyById } from "@/api/property/property";
+import Loader from "@/components/loader/loader";
 
 const PropertyPage = () => {
+  const [loading, setLoading] = useState(true);
   const params = useParams<{ id: string }>();
   const { id } = params;
 
@@ -24,68 +26,75 @@ const PropertyPage = () => {
         setProperty(data.data);
       } catch (error) {
         console.error("Error fetching property:", error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchProperty();
   }, [id]);
 
+  if (loading) {
+    return <Loader />;
+  }
   return (
-    <React.Fragment>
-      <div className="flex flex-col gap-5">
-        {/* property gallery */}
-        <PropertySlider
-          images={property?.images as { image: string; id: number }[]}
-        />
-        {/* detail title and location */}
-        <div>
+    property && (
+      <React.Fragment>
+        <div className="flex flex-col gap-5">
+          {/* property gallery */}
+          <PropertySlider
+            images={property?.images as { image: string; id: number }[]}
+          />
+          {/* detail title and location */}
           <div>
-            <span className="capitalize font-medium text-[var(--primary-color)]">
-              For {property?.type}
-            </span>
-            <h3 className="text-md md:text-lg font-medium text-gray-800">
-              <span className="capitalize">{property?.category?.name} </span>in{" "}
-              <span className="capitalize">{property?.location}</span>
-            </h3>
-          </div>
-          <div className="flex items-center gap-1">
-            <div className="text-gray-600 font-normal text-sm">
-              Rs.{property?.price} for {property?.duration}
-              <span className="capitalize">
-                {" "}
-                {property?.durationType?.toLowerCase()}
+            <div>
+              <span className="capitalize font-medium text-[var(--primary-color)]">
+                For {property?.type}
               </span>
+              <h3 className="text-md md:text-lg font-medium text-gray-800">
+                <span className="capitalize">{property?.category?.name} </span>
+                in <span className="capitalize">{property?.location}</span>
+              </h3>
             </div>
-            <LuDot className="text-gray-600" size={10} />
-            <div className="flex gap-1 items-center">
-              <GoStarFill size={10} className="text-slate-600" />
-              <span className="text-gray-600 text-sm">5.0</span>
+            <div className="flex items-center gap-1">
+              <div className="text-gray-600 font-normal text-sm">
+                Rs.{property?.price} for {property?.duration}
+                <span className="capitalize">
+                  {" "}
+                  {property?.durationType?.toLowerCase()}
+                </span>
+              </div>
+              <LuDot className="text-gray-600" size={10} />
+              <div className="flex gap-1 items-center">
+                <GoStarFill size={10} className="text-slate-600" />
+                <span className="text-gray-600 text-sm">5.0</span>
+              </div>
             </div>
           </div>
-        </div>
-        <Owner owner={property?.user as User} />
-        {/* services */}
-        <div className="service">
-          <div className="text-slate-800 font-medium flex flex-wrap gap-2">
-            {property?.Services &&
-              property?.Services?.map((service) => (
-                <div
-                  key={service}
-                  className="flex items-center gap-2 px-3 py-1 bg-gray-900/90 rounded-full shadow-sm text-white"
-                >
-                  <span className="text-sm">{service}</span>
-                </div>
-              ))}
-            {/* ... other services ... */}
+          <Owner owner={property?.user as User} />
+          {/* services */}
+          <div className="service">
+            <div className="text-slate-800 font-medium flex flex-wrap gap-2">
+              {property?.Services &&
+                property?.Services?.map((service) => (
+                  <div
+                    key={service}
+                    className="flex items-center gap-2 px-3 py-1 bg-gray-900/90 rounded-full shadow-sm text-white"
+                  >
+                    <span className="text-sm">{service}</span>
+                  </div>
+                ))}
+              {/* ... other services ... */}
+            </div>
           </div>
+          {/* description */}
+          <article className="text-[14px] text-slate-600 text-justify">
+            {property?.description}
+          </article>
+          {/* google map */}
+          <GoogleMap map={property?.map as string} />
         </div>
-        {/* description */}
-        <article className="text-[14px] text-slate-600 text-justify">
-          {property?.description}
-        </article>
-        {/* google map */}
-        <GoogleMap map={property?.map as string} />
-      </div>
-    </React.Fragment>
+      </React.Fragment>
+    )
   );
 };
 
