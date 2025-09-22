324a38062f721bc85b1b2a46b8662b3645c1a5bf
dammar093 <dammarrana093@gmail.com>
2025-09-22 14:24:34 +0545

    fixed service route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import app from "./app";
 import authRouter from "./routes/auth/auth.route";
 import userRouter from "./routes/user/user.route";
 import categoryRouter from "./routes/category/category.route";
-import serviceRouter from "./routes/service/service.route";
 import propertyRouter from "./routes/property/property.route";
 import wishlistRouter from "./routes/wishlist/wishlist.route";
 import { setupSwagger } from './utils/swagger';
@@ -22,7 +21,6 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/categories", categoryRouter);
-app.use("/api/v1/services", serviceRouter);
 app.use("/api/v1/properties", propertyRouter);
 app.use("/api/v1/wishlist", wishlistRouter);
 
