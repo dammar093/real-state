36531d6be7592854de139e8d0396676de98fbe68
dammar093 <dammarrana093@gmail.com>
2025-09-23 11:37:50 +0545

    added properties

--- frontend/src/app/(public)/properties/_components/index.tsx
+++ frontend/src/app/(public)/properties/_components/index.tsx
@@ -1,10 +1,9 @@
 "use client";
 import React, { useEffect } from "react";
-import { Col, Flex, Row, Spin } from "antd";
-import { useRouter } from "next/navigation";
+import { Col, Row } from "antd";
 import useProperties from "@/hooks/useProperties";
-import Link from "next/link";
 import PropertySlider from "@/components/property-sldier/property-slider";
+import Loader from "@/components/loader/loader";
 
 const PropertyWrapper = () => {
   const { loading, active, getActiveProperties } = useProperties();
@@ -12,26 +11,11 @@ const PropertyWrapper = () => {
     getActiveProperties();
   }, [getActiveProperties]);
 
-  if (loading) return <Spin />;
-
-  const slicedProperties = active?.properties?.slice(0, 8) || [];
+  if (loading) <Loader />;
 
   return (
-    <Row gutter={23}>
-      <Col xs={24} className="mb-4">
-        <Flex wrap justify="space-between" gap={4}>
-          <h2 className="text-sm md:text-lg lg:text-2xl font-semibold text-gray-800">
-            Explore Propeties
-          </h2>
-          <Link
-            href="/properties"
-            className="px-3 py-1 rounded !bg-[#800000] !text-white !hover:bg-[#a00000]"
-          >
-            View All
-          </Link>
-        </Flex>
-      </Col>
-      {slicedProperties.map((pr) => (
+    <Row gutter={23} className="mt-4">
+      {active?.properties?.map((pr) => (
         <Col xs={12} md={8} lg={6} key={pr?.id}>
           <div className="cursor-pointer hover:scale-[1.02] transition-transform duration-300 ease-in-out">
             <div className="flex flex-col gap-2">
